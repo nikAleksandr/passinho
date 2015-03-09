4971a4b5dde793a214f25d50be16335753d87a00
nikAleksandr <njlyell@gmail.com>
2015-03-08 20:37:19 -0400

    added a simple visualization of x y and z positions

--- js/main.js
+++ js/main.js
@@ -1,48 +1,68 @@
-
-
 var data = new p5.Table(),
 	numRows,
 	row = 1,
 	Pos,
 	PrevPos,
 	Vel,
-	Accel;
+	Accel,
+	scl = 0.000001;
 
 function preload(){
 	data = loadTable("data/LOG00119.TXT", "csv");
 }
 
 function setup(){
+	createCanvas(window.innerWidth, window.innerHeight);
+	
+	frameRate(100000);
+	
 	numRows = data.getRowCount();
-	Pos = createVector(0,0,0),
+	Pos = createVector(width/2,height/2,10),
 	PrevPos = Pos,
 	Vel = createVector(0,0,0),
 	Accel = createVector(0,0,0);
 	
 	//could set background color based on overall level of movement (dark blue to chilly blue, to yellow to bright orange)
 	background(255);
-	smooth();
 }
 
 function draw(){
+	//background(255);
 	//save previous position values before processing the next ones
 	PrevPos = Pos;
 
 	processData(row);
 	
+	noStroke();
+	fill(132, 55, 78, 5);
+	ellipse(Pos.x, Pos.y, Pos.z, Pos.z);
 	//line(PrevPos.x, PrevPos.y, Pos.x, Pos.y);
+	
+	//helper to print out the values of the vectors on screen
+	//text("x: " + nf(Accel.x, 0, 1, 2), width-100, 20);
+	//text("y: " + nf(Accel.y, 0, 1, 2), width-100, 40);
+	
+	
 	row ++;
-	if(row > numRows-1){
-		frameRate(0);
+	if(row >= numRows){
+		noLoop();
 	}
 }
 
-//processData will take in the raw accelerometer data and process it into x and y positions
+//processData will take in the raw accelerometer data and process it into x, y, and z positions
 function processData(row){
 	var xyz = data.getRow(row);
-	Pos.set(xyz.get(0), xyz.get(1), xyz.get(2));
-	
-	console.log(Pos);
-	
-	frameRate(1);
+	console.log("frameCount");
+	Accel.set(xyz.get(0), xyz.get(1), xyz.get(2));
+	//scale acceleration to reasonable pixel values
+	Accel.mult(scl);
+	Vel.add(Accel);
+	Pos.add(Vel);
 }
+//returns a random choice between 0, 1, and 2
+var rN = function(){
+	var r = random();	
+	if(r<.33) return 0;
+	else if(r<.66) return 1;
+	else return 2;
+};
